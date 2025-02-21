376ab6d461630dea93064f72704b86b42f696be1
Ujjwal Gaihre <ujjwalgaihre0@gmail.com>
2025-02-22 02:15:21 +0530

    Some change

--- backend/Routes/routes.js
+++ backend/Routes/routes.js
@@ -20,8 +20,9 @@ route.post('/register',async (req,res)=>{
         const response=await newuser.save()
         const payload={
             name:response.name,
-            password:response.password
         }
+        // Set token in an HTTP-only cookie
+     
         const token=generatetoken(payload)
         res.status(201).json({msg:"User created succesfully",user:response,token:token})
    
@@ -34,20 +35,24 @@ route.post('/login',async(req,res)=>{
   const {name,password}=req.body
   const user=await alluser.findOne({name})
   if(!user){
-    return res.status.json({error:"User does't exsit"})
+    return res.status(404).json({error:"User does't exsit"})
   }
   const ispaswordmatch=await user.comparePassword(password)
   if(!ispaswordmatch){
     return res.status(401).json({error:"Password does't match"})
   }
     const payload={
-     name:user.name,
-     password:user.password
+     name:user.name,  
 }
   const token=generatetoken(payload)
-  res.json({token})
+   res.json({token ,user:user})
     }catch(error){
         res.status(500).json({msg:"Server error"})
     }
+})
+
+route.get("/profile",(req,res)=>{
+   
+    res.json("user Info")
 })
 module.exports=route;
