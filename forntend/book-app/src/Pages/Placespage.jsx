4eaf15743074b6bdf1c136d876f86e84f62bc0bd
Ujjwal Gaihre <ujjwalgaihre0@gmail.com>
2025-03-03 14:23:36 +0530

    Image and Upload error fix

--- forntend/book-app/src/Pages/Placespage.jsx
+++ forntend/book-app/src/Pages/Placespage.jsx
@@ -3,7 +3,7 @@ import { FaPlus } from "react-icons/fa";
 import {FaCloudUploadAlt} from "react-icons/fa";
 import { useState } from "react";
 import Preks from "./Perks";
-;
+import axios from 'axios';
 export default function PlacesPage(){
     const {action}=useParams()
     const [title,setTitle]=useState("")
@@ -18,9 +18,36 @@ export default function PlacesPage(){
     const [maxguests,setMaxguests]=useState(1)
     
     // function to upload to photo by link
-    function uploadphotobylink(){
-
+     const  uploadphotobylink=async(e)=>{
+        e.preventDefault()
+        try{
+            const response=await  axios.post('/upload-by-links' ,{link:photolnik})
+            const newphoto=response.data;
+             setaddPhoto((prev) => [...prev, newphoto]);
+                setLink("")
+            }catch(error){
+                console.error("Upload failed:", error.response ? error.response.data : error.message);
+    
+            }
     }
+    // for the upload photo
+    function uploadphoto(event){
+    const files=event.target.files;
+    const data=new FormData()
+    for(let i=0; i<files.length;i++){
+        data.append('photos',files[i]);
+    }
+    axios.post('/upload',data,{
+    // headers:{'Content-Type':'multipart/form-data'}
+    }).then((response)=>{
+        const {data:newphoto}=response;
+        setaddPhoto((prev) => [...prev, ...newphoto]);
+    }).catch((error)=>{
+        console.log("Error at Upload",error);
+
+    })
+      
+}
  return(
 <div>
     {action !=='new'&&(
@@ -44,13 +71,29 @@ export default function PlacesPage(){
 
       <h2 className="text-xl mt-2  font-serif">Photo</h2>
       <p className="text-gray-500 text-sm ">Photo of this Place </p>
+
       <div className="flex gap-2 items-center ">
         <input type="text" value={photolnik} 
         onChange={(e)=>setLink(e.target.value)} placeholder={"Add using link...... jpg"} />
-        <button className="bg-gray-300 h-12 w-auto  px-6  rounded-2xl ">Add&nbsp;Photo</button>
+        <button  onClick={uploadphotobylink}
+        className="bg-gray-300 h-12 w-auto  px-6  rounded-2xl ">Add&nbsp;Photo</button>
       </div>
-      <div className="mt-2 grid  grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
-      <button className="border flex justify-center items-center  gap-2 bg-transparent rounded-2xl px-2 py-4 shadow-md text-slate-950 "> <FaCloudUploadAlt /> Upload Photo</button>
+
+          <div className="mt-2 grid items-center  grid-cols-4 md:grid-cols-5 lg:grid-cols-6">
+      {addPhoto.length > 0 && addPhoto.map((link, index) => (
+          <div key={index} className="py-2 px-2 ">
+         <img   src={link.includes('http') ? link : 
+             (link.includes('.jpg') ? `http://localhost:3000/images/${link}` : 
+              `http://localhost:3000/uploads/${link}`)}
+        alt="Uploaded" 
+        className="w-36 h-40 object-cover rounded-lg"  />
+    </div>
+))}
+
+      <label className="border w-36 h-12 cursor-pointer   flex justify-center items-center  gap-2 bg-transparent rounded-xl px-2 py-4 shadow-sm text-slate-950 "> 
+       <input type="file"   encType="multipart/form-data" className="hidden" onChange={uploadphoto}/>
+        <FaCloudUploadAlt />
+      Upload Photo</label> 
       </div>
       <h2 className="text-xl  font-serif">Description</h2>
       <p className="text-gray-500 text-sm ">Description of the Place</p>
