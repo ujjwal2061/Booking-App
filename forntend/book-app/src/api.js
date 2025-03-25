6df1774b83bec983b784071fb6568f6aa6c17ed9
Ujjwal Gaihre <ujjwalgaihre0@gmail.com>
2025-03-25 21:41:01 +0530

    Thsdi fuck

--- forntend/book-app/src/api.js
+++ forntend/book-app/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
  const api=axios.create({
-    baseURL: "https://booking-app-afjh.vercel.app/"||"http://localhost:3000",
+    baseURL: import.meta.env.VITE_API_BASE_URL||"http://localhost:3000",
     withCredentials:true
  })
  export default api
