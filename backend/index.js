92af6379cbc9e5c8fd89174b2b8b237462f450a3
Ujjwal Gaihre <ujjwalgaihre0@gmail.com>
2025-03-25 21:17:38 +0530

    the vercel is fuck

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ require('dotenv').config();
 
 const PORT=3000;
 app.use(cors({
-    origin:['',
+    origin:['https://booking-app-ecru-chi.vercel.app/',
         "http://localhost:5173",  ],
         credentials:true,
         methods:["GET","POST","PUT","DELETE"],
