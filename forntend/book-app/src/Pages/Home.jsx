da1b64936cc23f9a66d086556acba75a3963fe43
Ujjwal Gaihre <ujjwalgaihre0@gmail.com>
2025-05-01 13:32:36 +0530

    Change UI and Route

--- forntend/book-app/src/Pages/Home.jsx
+++ forntend/book-app/src/Pages/Home.jsx
@@ -10,16 +10,19 @@ export default function Home() {
   const { user, ready } = useContext(UserContext);
 
   if (!ready) {
-    return <div>Loading...</div>;
+    return <div className="h-screen flex justify-center items-center">
+    <h1 className="text-xl sm:text-4xl md:text-5xl font-semibold underline text-center animate-fade-in-pulse">
+      Homy Find Your Next House
+    </h1>
+  </div>
   }
 
   if (user) {
     return <Navigate to="/allplaces" replace />;
   }
   return (
-    <section className='min-h-screen w-full '>
-
-    <div className='flex flex-col md:flex-row justify-center items-center mt-2 px-4 md:px-10 py-10'>
+    <section className=' w-full '>
+    <div className='flex  flex-col md:flex-row justify-center items-center mt-2 px-4 md:px-10 py-10'>
       <div className='md:w-1/2 w-full px-4 md:px-10 py-8 text-center md:text-left'>
         <h1 className='text-4xl md:text-5xl font-serif'>Explore your <br />place to stay</h1>
         <p className='mt-4 font-serif text-gray-700'>
@@ -40,22 +43,19 @@ export default function Home() {
         <div className='py-4 hidden md:block'>
           <h2 className='font-mono px-6 py-2 '>Be With Nature</h2>
           <div className='w-full md:w-1/2 flex   justify-center '>
-            <img src={Moutainjpg} className='w-96 h-80 md:w-72 md:h-72  object-cover rounded-lg' alt="Nature" />
+            <img src={Moutainjpg} loading="lazy" alt="Nature" />
           </div>
         </div>
       </div>
       <div className='w-full md:w-1/2 '>
-        <img src={Homejpg} className='w-full h-auto md:h-[600px] object-cover md:-mb-6 rounded-md' alt="Home" />
+        <img src={Homejpg} loading='lazy' className='w-full h-auto md:h-[600px] object-cover md:-mb-6 rounded-md' alt="Home" />
          <div className='bg-black mt-4 md:mt-9 flex justify-between px-6 rounded-lg py-2 text-white font-serif'>
           <h1>1,000+</h1>
           <p>Unique Places</p>
          </div>
         
       </div>
-    </div> 
- 
-
-                
+    </div>             
 </section>
 );
 }
